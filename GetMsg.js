f58ad77ffc70523794316096e3102f72db2a925c
Sakshi Jain <sugareyed@gmail.com>
2017-04-12 18:34:08 +0530

    To receive the message from the user

--- GetMsg.js
+++ GetMsg.js
@@ -1,12 +1,9 @@
-// Download the Node helper library from twilio.com/docs/node/install
-// These vars are your accountSid and authToken from twilio.com/user/account
 var accountSid = 'AC2b4966e2d5a02877a4bdd1cc85920b54';
 var authToken = "7d3035bd6b546261a0176c884b22bedd";
 var client = require('twilio')(accountSid, authToken);
 
 var filterOpts = {
-    //to: '(503) 451-6565',
-    from: '919740877787',
+     from: '919740877787',
     //dateSent: new Date(2017, 04, 11)
 };
 
